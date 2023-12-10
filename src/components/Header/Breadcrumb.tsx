50343390172f62f3ca221b1decb00e071d0287e0
Tannaz Shirzadi <tia.shr97@gmail.com>
2023-12-10 09:50:57 +0300

    feat: create header and breadcrumb

---
+++ src/components/Header/Breadcrumb.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+interface BreadcrumbProps {
+  pathArray: string[];
+}
+
+export default function Breadcrumb({ pathArray }: BreadcrumbProps) {
+  return (
+    <ul className="flex">
+      <li className="mr-2 hover:text-alpha">
+        <Link href="/">Home</Link>
+      </li>
+      {pathArray.map((path: string, index: number) => {
+        if (!path) {
+          return;
+        }
+        const href = pathArray
+          .slice(0, index + 1)
+          .map((p: string) => p && "/" + p)
+          .toString()
+          .replaceAll(",", "");
+        return (
+          <li key={href}>
+            {" "}
+            /
+            <Link className="capitalize mx-2" href={href}>
+              {path}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,19 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import Breadcrumb from "./Breadcrumb";
 
-//
-// router.back()
 export default function Header() {
   const router = useRouter();
+  const pathname = usePathname();
+  const pathArray: string[] = pathname.split("/");
   return (
-    <div className="bg-beta p-3 text-dark-primary dark:text-light-primary mb-2">
-      this is header
+    <div className="flex items-center bg-beta p-2 text-dark-primary dark:text-light-primary mb-2">
+      <button className="mr-4" onClick={() => router.back()}>
+        <IoMdArrowRoundBack size="1.25rem" />
+      </button>
+      <Breadcrumb pathArray={pathArray} />
     </div>
   );
 }
