a69efc6760bf9414d86943c96528c54279acbbc2
Tannaz Shirzadi <tia.shr97@gmail.com>
2023-12-24 11:50:36 +0300

    feat: add fields to creat goal form

--- src/components/GoalForm/index.tsx
+++ src/components/GoalForm/index.tsx
@@ -20,7 +20,16 @@ const GoalForm = ({ initialState }: GoalFormProps) => {
     <form className="fle flex-col gap-2 w-[30rem] mx-12" action={formAction}>
       <Input title="title" type="text" name="title" />
       <Input title="description" name="description" isTextArea />
-
+      <Input title="Estimated Date" type="date" name="date" />
+      <label
+        className="flex x justify-between capitalize p-2 gap-2 mb-2 items-center 
+  border-b border-orange text-dark-blue"
+      >
+        Sub Tasks:
+        <button className="border border-orange rounded px-4 text-purple text-xl w-72">
+          +
+        </button>
+      </label>
       {state?.message}
       <SubmitButton />
     </form>
--- src/components/SubmitButton/index.tsx
+++ src/components/SubmitButton/index.tsx
@@ -7,7 +7,11 @@ export function SubmitButton() {
   const { pending } = useFormStatus();
 
   return (
-    <button type="submit" aria-disabled={pending}>
+    <button
+      className="bg-rose font-semibold py-2 px-3 flex ml-auto rounded border text-orange border-orange"
+      type="submit"
+      aria-disabled={pending}
+    >
       Add
     </button>
   );
